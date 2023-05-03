bcae898a953f8a7fac681de0ef36d6595b02edc6
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-05-03 19:25:49 +0100

    installed mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@/lib/theme';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
